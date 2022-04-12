3d15912d09de11f11fcbecdcb6e55930c66fea94
Aleksander Pucher <pucher.aleksander@gmail.com>
2022-04-12 20:55:20 +0200

    Initial commit.

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KITO_URL=https://github.com/be-accurate/kito.git
+NGINX_URL=https://github.com/be-accurate/nginx.git
+
+KITO_DIR=kito
+NGINX_DIR=nginx
+
+# order is important, so the loop works as intented
+urls=("${KITO_URL}" "${NGINX_URL}")
+dirs=("${KITO_DIR}" "${NGINX_DIR}")
+
+for i in "${!urls[@]}"; do
+    if [[ ! -d "${dirs[i]}" ]]; then
+        git clone "${urls[i]}" "${dirs[i]}"
+    fi
+done
